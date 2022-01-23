01b25fdf4820fb13436c8d6766bbf452495a933b
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2022-01-23 11:29:17 +0530

    add purge feature

---
+++ cmd/purge.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// purgeCmd represents the purge command
+var purgeCmd = &cobra.Command{
+	Use:   "purge",
+	Short: "purge specific task ",
+	Long: `Command helps you purging specific task from cancelled list. For example:
+
+	Note : you will only be able to purge a task from cancelled list. If you want
+		   to do so with current task list. Please first cancel it then purge.
+		   
+		   for example : 
+		     		   -> sparkle purge [index of the task]
+		   				  sparkle purge 1
+	
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			cmd.Help()
+			return
+		}
+
+		idx, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Errorf("Please pass valid argument - ", err)
+			cmd.Help()
+			return
+		}
+		datafile.PurgeTask(idx)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(purgeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// purgeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// purgeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
